c0f3128147e70b9095b8f920d3c95b737c3b5621
Dumebi Okolo <okolodumebi@gmail.com>
2022-11-27 20:35:04 +0100

    fix render issues

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Box from './components/Box';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import timeout from './utils/util';
+
 
 function App() {
 
+    var buttonColors = ["red", "green", "orange", "magenta"];
+    var userClickedPattern = [];
+    var randomColor;
+    var gamePattern;
+    var displayColors;
 
-  const [header, setHeader] = useState("PRESS ANY KEY TO START");
+    const [isOn, setisOn] = useState(false);
+    
+    function startGame() {
+        setisOn(true)
+    }
+
+    const [gameStart, setGameStart] = useState({
+        gameStarted: false,
+        colors: [],
+        userColors: [],
+        onDisplay: false,
+        score: 0
+    })
+
+    useEffect(() => {
+      if (isOn) {
+        setGameStart({
+            ...gameStart, gameStarted: true
+        })
+      }
+      else {
+        setGameStart(gameStart)
+      }
+    }, [isOn])
+
+    useEffect(() => {
+      if (isOn && gameStart.onDisplay) {
+        randomColor = buttonColors[Math.floor(Math.random()*4)]
+        gamePattern = [...gameStart.colors]
+        gamePattern.push(randomColor)
+        setGameStart({...gameStart, colors: gamePattern})
+      }
+    }, [isOn, gameStart.onDisplay])
+
+    useEffect(() => {
+      if (isOn && gameStart.onDisplay && gameStart.colors.length) {
+        displayColors()
+      }
+    }, [isOn, gameStart.onDisplay, gameStart.colors.length])
+
+    const [flashColor, setFlashColor] = useState("");
+    
+
+  const [header, setHeader] = useState("PRESS ANY KEY TO START")
+
+
  
 
   return (
-
+    
     <div >
       <Header
       text={header} />
       <div>
-        <Box />
-
+      { !isOn && !gameStart.score && (<button className='start-button' type='button' onClick={startGame}>
+            Start Game
+        </button>)}
+        {isOn && (gameStart.onDisplay || gameStart.userColors) && (
+          <div className="score">{gameStart.score}</div>
+        )}
+      </div>
+      <div>
+      <h1>
+        Dumebi is the greatest!
+      </h1>
+      {buttonColors.map((v, i) => (
+        <Box 
+          //  onClick={() => {
+          //   cardClickHandle(v);
+          //       }}
+            flash={flashColor === v}
+            color={v} />
+      ))
+            }
       </div>
     </div>
    
